73137a92c5bf7bb0d0e1834a632dbfbfd5ac452b
gungdeaditya <gunkdep@gmail.com>
2017-06-06 22:53:14 +0700

    modify list adapter

--- app/src/main/java/com/example/gungde/imk_m3/adapter/MainListAdapter.java
+++ app/src/main/java/com/example/gungde/imk_m3/adapter/MainListAdapter.java
@@ -1,7 +1,6 @@
 package com.example.gungde.imk_m3.adapter;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
